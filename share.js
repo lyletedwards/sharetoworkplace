96088081b6d855cd456f07807758ba9d1d239ead
Lyle Edwards <40928309+lyletedwards@users.noreply.github.com>
2018-07-08 00:03:38 -0400

    Create share.js

---
+++ share.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+url = 'https://work.facebook.com/sharer.php?display=popup&u=' + window.location.href;
+options = 'toolbar=0,status=0,resizable=1,width=626,height=436';
+window.open(url,'sharer',options);
+})();
