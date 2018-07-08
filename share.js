39013eb287fe069569794d7f66e518cef60ad483
Lyle Edwards <40928309+lyletedwards@users.noreply.github.com>
2018-07-08 00:43:41 -0400

    Update share.js
    copy this javascript to a bookmark URL

--- share.js
+++ share.js
@@ -2,4 +2,4 @@ javascript:(function(){
 url = 'https://work.facebook.com/sharer.php?display=popup&u=' + window.location.href;
 options = 'toolbar=0,status=0,resizable=1,width=626,height=436';
 window.open(url,'sharer',options);
-})();
+})(); 
